970dabbb7964960e4fce6c9979cbfda8e30b6e81
muhammad.saeed@dbqplabs.com <joinsaeeddev@gmail.com>
2025-04-11 18:47:44 +0500

    Update dependcies

--- src/components/dashboard/AddTermForm.tsx
+++ src/components/dashboard/AddTermForm.tsx
@@ -5,9 +5,6 @@ import { SchoolFormValues } from "@/features/school/types";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { format } from "date-fns";
-import { useState } from "react";
-import { DateRange } from "react-day-picker";
 export default function AddTermForm({
   onSubmit,
 }: {
@@ -24,22 +21,6 @@ export default function AddTermForm({
       academicYear: "",
     },
   });
-
-  const [dateRange, setDateRange] = useState<DateRange>({
-    from: new Date(), // Ensure 'from' is always set
-    to: new Date(new Date().getFullYear() + 1, 5, 30), // Default to end of next academic year
-  });
-
-  // Update academic year on date change
-  const handleDateChange = (range: DateRange | undefined) => {
-    if (range?.from && range?.to) {
-      setDateRange(range);
-      setValue(
-        "academicYear",
-        `${format(range.from, "yyyy")}-${format(range.to, "yyyy")}`
-      );
-    }
-  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="bg-white space-y-4">
