a7e3ad774e5c45b7ecd4a97ea43f6e1be59881dc
muhammad.saeed@dbqplabs.com <joinsaeeddev@gmail.com>
2025-04-29 15:44:45 +0500

    make the auth state persistent

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,9 +1,15 @@
-// app/providers.tsx
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import { store, persistor } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
