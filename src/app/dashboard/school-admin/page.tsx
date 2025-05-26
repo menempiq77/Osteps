f6cd063eed5fa77bef70e7ed1db1fb4dcf6c2e0f
muhammad.saeed@dbqplabs.com <joinsaeeddev@gmail.com>
2025-05-26 12:03:08 +0500

    school admin page changes for build

--- src/app/dashboard/school-admin/page.tsx
+++ src/app/dashboard/school-admin/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
-import ClassManager from "@/components/dashboard/ClassManager";
+import React from 'react'
 
-export default function SchoolAdminDashboard() {
-  const { currentUser } = useSelector((state: RootState) => state.auth);
-  const { classes } = useSelector((state: RootState) => state.class);
-
-  const schoolClasses = classes.filter(
-    (c) => c.schoolId === currentUser?.schoolId
-  );
-
+export default function page() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">School Admin Dashboard</h1>
-      <ClassManager schoolId={currentUser?.schoolId || ""} />
-      {/* Term Configuration Component */}
-    </div>
-  );
+    <div>School admin page</div>
+  )
 }
