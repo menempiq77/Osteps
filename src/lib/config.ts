618532900d60fb097cf1af6037a0660d70e07f3d
muhammad.saeed@dbqplabs.com <joinsaeeddev@gmail.com>
2025-05-26 11:53:55 +0500

    Update base Url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 // src/lib/config.ts
-export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://54.66.166.198/api';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://dashboard.osteps.com/api';
