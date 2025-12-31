8fbd2fc759e25768047f3686b5409d57e7c9b8b7
muhammad.saeed@dbqplabs.com <joinsaeeddev@gmail.com>
2025-12-31 10:44:51 +0500

    revert to live base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,7 @@
 // src/lib/config.ts
-export const API_BASE_URL = 'http://osteps-bakcend.test/api';
-export const IMG_BASE_URL = 'http://osteps-bakcend.test';
+// export const API_BASE_URL = 'http://osteps-bakcend.test/api';
+// export const IMG_BASE_URL = 'http://osteps-bakcend.test';
+
+// src/lib/config.ts
+export const API_BASE_URL = 'https://dashboard.osteps.com/api';
+export const IMG_BASE_URL = 'https://dashboard.osteps.com';
